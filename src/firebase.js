1d6032bdf08d2ca9184ece8545dd45cd8ba6adb9
Adam Hartman <adamhartman5@gmail.com>
2023-03-12 01:57:52 -0500

    Initialize build and perform clean up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7CfOmkJLqhwlCeMKC7_Qz2zK7C3s9Dck",
+  authDomain: "netflix-clone-c40d1.firebaseapp.com",
+  projectId: "netflix-clone-c40d1",
+  storageBucket: "netflix-clone-c40d1.appspot.com",
+  messagingSenderId: "740802135330",
+  appId: "1:740802135330:web:f1bada17c0f606ba67917b"
+};
