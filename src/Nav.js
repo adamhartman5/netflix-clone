d55f313ebf8349e4bf9f41eb2027fb6d250f6acc
Adam Hartman <adamhartman5@gmail.com>
2023-03-12 22:20:54 -0400

    Start working on nav

--- src/Nav.js
+++ src/Nav.js
@@ -5,12 +5,14 @@ import netflix from './images/netflix.png';
 function Nav() {
 	return (
 		<div className='nav'>
-			<img src={netflix} alt='' />
-			<img
-				src='https://cdn.discordapp.com/avatars/551956038459457539/943d6c9d60c052884d158d8d8faac6eb.png?size=240'
-				alt=''
-			/>
-			<h1>This is the nav</h1>
+			<div className="nav__contents">
+				<img className='nav__logo' src={netflix} alt='' />
+				<img
+					className='nav__avatar'
+					src='https://cdn.discordapp.com/avatars/551956038459457539/943d6c9d60c052884d158d8d8faac6eb.png?size=240'
+					alt=''
+				/>
+			</div>
 		</div>
 	);
 }
