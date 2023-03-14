019645ef1fddfa146740f829cb390a768360082f
Adam Hartman <adamhartman5@gmail.com>
2023-03-14 00:23:21 -0400

    Finish nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.css';
 import netflix from './images/netflix.png';
 
 function Nav() {
+	const [show, handleShow] = useState(false);
+
+	const transitionNavBar = () => {
+		if (window.scrollY > 100) {
+			handleShow(true);
+		} else {
+			handleShow(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', transitionNavBar);
+		return () => window.removeEventListener('scroll', transitionNavBar);
+	}, []);
 	return (
-		<div className='nav'>
-			<div className="nav__contents">
+		<div className={`nav ${show && "nav__black"}`}>
+			<div className='nav__contents'>
 				<img className='nav__logo' src={netflix} alt='' />
 				<img
 					className='nav__avatar'
