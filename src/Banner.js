03241d3894f6a28ab9dc7209f2947e58c810695b
Adam Hartman <adamhartman5@gmail.com>
2023-03-14 00:31:07 -0400

    Create banner files and add bg image

---
+++ src/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css'
+
+function Banner() {
+	return <header className='banner' style={{
+		backgroundSize: 'cover',
+		backgroundImage: `url('https://www.xfire.com/wp-content/uploads/2022/09/netflix-cover-2-800x450.png')`,
+		backgroundPosition: 'center center',
+	}}>
+
+	</header>;
+}
+
+export default Banner;
