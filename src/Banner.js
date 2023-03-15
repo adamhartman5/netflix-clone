28a390eb3ce826abb12c731bc05a270e5c237b29
Adam Hartman <adamhartman5@gmail.com>
2023-03-15 01:01:08 -0400

    Add dynamic movie to banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.css';
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
+	const [movie, setMovie] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(requests.fetchNetFlixOriginals);
+			setMovie(
+				request.data.results[
+					Math.floor(Math.random() * request.data.results.length - 1)
+				]
+			);
+			return request;
+		}
+		
+		fetchData();
+	}, []);
+
 	function truncate(string, n) {
 		return string?.length > n ? string.substr(0, n - 1) + '...' : string;
 	}
@@ -11,7 +29,7 @@ function Banner() {
 			className='banner'
 			style={{
 				backgroundSize: 'cover',
-				backgroundImage: `url('https://www.xfire.com/wp-content/uploads/2022/09/netflix-cover-2-800x450.png')`,
+				backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
 				backgroundPosition: 'center center',
 			}}
 		>
--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = '53e57209b72bcb84de0965d220efcd46';
 
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
